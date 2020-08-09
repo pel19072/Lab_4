5b996be969fdf6e2b80dc0afde780a7dfc48ba64
pel19072 <68125920+pel19072@users.noreply.github.com>
2020-08-08 21:12:24 -0600

    Code
    Se agregó el código final

---
+++ Test_Bench_tb.v
@@ -0,0 +1,127 @@
+// Esto es para aquellos que están usando iverilog de compilador
+// Para los que usan apio pueden obviarlo
+`include "codigo.v"
+// ` -> backtick
+// ' -> comilla simple
+
+
+module testbench();
+
+  // Inputs para Gate_Level
+  reg p1, p2, p3, p4, p5, p6;
+  // Inputs para Operators
+  reg i1, i2, i3, i4, i5, i6, i7, i8;
+  
+  // Outputs para Gate_Level
+  wire led1, led2, led3, led4;
+  // Outputs para Operators
+  wire led5, led6, led7, led8;
+
+  gateLevel_1   G1(p1, p2, p3, led1);
+  gateLevel_2   G2(p1, p2, p3, led2);
+  gateLevel_3   G3(p4, p5, p6, led3);
+  gateLevel_4   G4(p4, p5, p6, led4);
+  
+  
+  operadores_1 OP1(i1, i2, i3, i4, led5);
+  operadores_2 OP2(i1, i2, i3, i4, led6);
+  operadores_3 OP3(i5, i6, i7, i8, led7);
+  operadores_4 OP4(i5, i6, i7, i8, led8);
+
+  
+  // Gate Level Modelling --> Ejercicio 1: Tablas 1 y 2 
+  initial begin
+	#1
+    $display("Ejercicio 1: Tablas 1 y 2");
+    $display("A B C | Y1 Y2");
+    $display("------|------");
+    $monitor("%b %b %b | %b  %b", p1, p2, p3, led1, led2);
+       p1 = 0; p2 = 0; p3 = 0;
+    #1 p1 = 0; p2 = 0; p3 = 1;
+    #1 p1 = 0; p2 = 1; p3 = 0;
+    #1 p1 = 0; p2 = 1; p3 = 1;
+    #1 p1 = 1; p2 = 0; p3 = 0;
+    #1 p1 = 1; p2 = 0; p3 = 1;
+    #1 p1 = 1; p2 = 1; p3 = 0;
+    #1 p1 = 1; p2 = 1; p3 = 1;
+  end
+  
+  // Gate Level Modelling --> Ejercicio 2: Tablas 2 y 4
+  initial begin
+    #10
+    $display("\n");
+	$display("Ejercicio 2: Tablas 2 y 4");
+    $display("A B C | Y1 Y2");
+    $display("------|--");
+    $monitor("%b %b %b | %b %b", p4, p5, p6, led3, led4);
+       p4 = 0; p5 = 0; p6 = 0;
+    #1 p4 = 0; p5 = 0; p6 = 1;
+    #1 p4 = 0; p5 = 1; p6 = 0;
+    #1 p4 = 0; p5 = 1; p6 = 1;
+    #1 p4 = 1; p5 = 0; p6 = 0;
+    #1 p4 = 1; p5 = 0; p6 = 1;
+    #1 p4 = 1; p5 = 1; p6 = 0;
+    #1 p4 = 1; p5 = 1; p6 = 1;
+  end
+  
+  // Operators --> Ejercicio 1: Tablas 3 y 4
+  initial begin
+	#18
+	$display("\n");
+    $display("Ejercicio 1: Tablas 3 y 4");
+    $display("A B C D | Y1 Y2");
+    $display("--------|------");
+    $monitor("%b %b %b %b | %b  %b", i1, i2, i3, i4, led5, led6);
+       i1 = 0; i2 = 0; i3 = 0; i4 = 0;
+    #1 i1 = 0; i2 = 0; i3 = 0; i4 = 1;
+    #1 i1 = 0; i2 = 0; i3 = 1; i4 = 0;
+    #1 i1 = 0; i2 = 0; i3 = 1; i4 = 1;
+    #1 i1 = 0; i2 = 1; i3 = 0; i4 = 0;
+    #1 i1 = 0; i2 = 1; i3 = 0; i4 = 1;
+    #1 i1 = 0; i2 = 1; i3 = 1; i4 = 0;
+    #1 i1 = 0; i2 = 1; i3 = 1; i4 = 1;
+	#1 i1 = 1; i2 = 0; i3 = 0; i4 = 0;
+    #1 i1 = 1; i2 = 0; i3 = 0; i4 = 1;
+    #1 i1 = 1; i2 = 0; i3 = 1; i4 = 0;
+    #1 i1 = 1; i2 = 0; i3 = 1; i4 = 1;
+    #1 i1 = 1; i2 = 1; i3 = 0; i4 = 0;
+    #1 i1 = 1; i2 = 1; i3 = 0; i4 = 1;
+    #1 i1 = 1; i2 = 1; i3 = 1; i4 = 0;
+    #1 i1 = 1; i2 = 1; i3 = 1; i4 = 1;
+  end
+  
+  // Gate Level Modelling --> Ejercicio 2: Tablas 2 y 4
+  initial begin
+    #35
+    $display("\n");
+	$display("Ejercicio 2: Tablas 1 y 3");
+    $display("A B C D | Y1 Y2");
+    $display("--------|------");
+    $monitor("%b %b %b %b | %b  %b", i5, i6, i7, i8, led7, led8);
+       i5 = 0; i6 = 0; i7 = 0; i8 = 0;
+    #1 i5 = 0; i6 = 0; i7 = 0; i8 = 1;
+    #1 i5 = 0; i6 = 0; i7 = 1; i8 = 0;
+    #1 i5 = 0; i6 = 0; i7 = 1; i8 = 1;
+    #1 i5 = 0; i6 = 1; i7 = 0; i8 = 0;
+    #1 i5 = 0; i6 = 1; i7 = 0; i8 = 1;
+    #1 i5 = 0; i6 = 1; i7 = 1; i8 = 0;
+    #1 i5 = 0; i6 = 1; i7 = 1; i8 = 1;
+	#1 i5 = 1; i6 = 0; i7 = 0; i8 = 0;
+    #1 i5 = 1; i6 = 0; i7 = 0; i8 = 1;
+    #1 i5 = 1; i6 = 0; i7 = 1; i8 = 0;
+    #1 i5 = 1; i6 = 0; i7 = 1; i8 = 1;
+    #1 i5 = 1; i6 = 1; i7 = 0; i8 = 0;
+    #1 i5 = 1; i6 = 1; i7 = 0; i8 = 1;
+    #1 i5 = 1; i6 = 1; i7 = 1; i8 = 0;
+    #1 i5 = 1; i6 = 1; i7 = 1; i8 = 1;
+  end
+  
+  initial
+    #52 $finish;
+  
+  initial begin
+    $dumpfile("Test_Bench_tb.vcd");
+    $dumpvars(0, testbench);
+  end
+  
+endmodule
---
+++ codigo.v
@@ -0,0 +1,113 @@
+//-----------------------------------
+// Ricardo Pellecer Orellana
+// Carné: 19072
+// 9/Agosto/2020
+// Gate Level Modelling 
+// Tablas del Ejercicio 01: Tabla 01 y Tabla 02 
+// Tablas del Ejercicio 02: Tabla 02 y Tabla 04
+// Operadores
+// Tablas del Ejercicio 01: Tabla 03 y Tabla 04
+// Tablas del Ejercicio 02: Tabla 01 y Tabla 03
+// -----------------------------------
+
+// Ejercicio 1 Tabla 1;
+
+module gateLevel_1(input wire A, B, C, output wire Y);
+
+  wire notA, notB, notC, or1, or2;
+
+  // Not's 
+  
+  not (notA, A);
+  not (notB, B);
+  not (notC, C);
+  
+  // Or's
+  
+  or  (or1, A, notC);
+  or  (or2, notA, notB, C);
+  
+  // And's
+  
+  and (Y, or1, or2);
+  
+endmodule
+
+// Ejercicio 1 Tabla 2;
+
+module gateLevel_2(input wire A, B, C, output wire Y);
+
+  wire notA, notB, notC, or1, or2;
+
+  // Not's 
+  
+  not (Y, B);
+  
+endmodule
+
+// Ejercicio 2 Tabla 2;
+
+module gateLevel_3(input wire A, B, C, output wire Y);
+
+  wire notA, notB, notC, or1, or2;
+
+  // Not's 
+  
+  not (notB, B);
+  
+  // Or's
+  
+  or  (Y, notB, C);
+    
+endmodule
+
+// Ejercicio 2 Tabla 4;
+
+module gateLevel_4(input wire A, B, C, output wire Y);
+
+  wire notA, notB, notC, or1, or2;
+
+  // Not's 
+  
+  not (notA, A);
+  not (notC, C);
+  
+  // Or's
+  
+  or  (or1, notA, B);
+  or  (or2, B, notC);
+  
+  // And's
+  
+  and (Y, or1, or2);
+  
+endmodule
+
+
+// Ejercicio 1 Tabla 3
+module operadores_1(input wire A, B, C, D, output wire Y);
+
+  assign Y = (~A & ~B & ~C & ~D) | (~A & ~B & C & D) | (~A & B & ~C & D) | (~A & B & C & ~D) | (A & B & ~C & ~D) | (A & B & C & D) | (A & ~B & ~C & D) | (A & ~B & C & ~D);
+
+endmodule
+
+// Ejercicio 1 Tabla 4
+module operadores_2(input wire A, B, C, D, output wire Y);
+
+  assign Y = A & (~A | B | C | ~D);
+
+endmodule
+
+// Ejercicio 2 Tabla 1
+module operadores_3(input wire A, B, C, D, output wire Y);
+
+  assign Y = (~B & ~C & ~D) | (A & ~C) | (A & ~B) | (A & C & ~D);
+
+endmodule
+
+// Ejercicio 2 Tabla 3
+module operadores_4(input wire A, B, C, D, output wire Y);
+
+  assign Y = (B | D) & (A | B | ~C);
+  
+endmodule
